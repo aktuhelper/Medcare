0fa8f37ac0a5a569aa770216d9a516d4a5a04cf8
Khushbu Singh <singhkhushbu3200@gmail.com>
2025-10-23 22:31:56 +0530

    base

--- app/_utils/GlobalAPI.jsx
+++ app/_utils/GlobalAPI.jsx
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: "https://medcare-beta.vercel.app/api",
   headers: {
     Authorization: `Bearer ${ API_KEY }`,
   },
